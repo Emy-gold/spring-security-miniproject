e518f88e1373c61a5ced2452878131ef374eeb02
Emy-gold <imanelgarate@gmail.com>
2026-03-08 15:49:45 +0000

    set the configuration

---
+++ src/main/java/org/codetutorial/springsecurityminiproject/SpringSecurityMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package org.codetutorial.springsecurityminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMiniprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMiniprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codetutorial/springsecurityminiproject/SpringSecurityMiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codetutorial.springsecurityminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMiniprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
