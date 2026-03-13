566abb231e80969c57cb033bab1eb89620c9c04b
Emy-gold <imanelgarate@gmail.com>
2026-03-13 21:49:19 +0000

    add the dto classes and register

--- src/main/java/org/codetutorial/springsecurityminiproject/controller/AuthController.java
+++ src/main/java/org/codetutorial/springsecurityminiproject/controller/AuthController.java
@@ -1,13 +1,43 @@
 package org.codetutorial.springsecurityminiproject.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.codetutorial.springsecurityminiproject.dto.RegisterRequest;
+import org.codetutorial.springsecurityminiproject.model.User;
+import org.codetutorial.springsecurityminiproject.repository.UserRepository;
+import org.codetutorial.springsecurityminiproject.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    @Autowired private AuthenticationManager authManager;
+    private final AuthenticationManager authManager;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest req){
+        if (userRepository.findByUsername(req.getUsername()).isPresent())
+            return  ResponseEntity.badRequest().body("Username deja prix !");
+
+        User user = new User();
+        user.setUsername(req.getUsername());
+        user.setPassword(passwordEncoder.encode(req.getPassword()));
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+        return ResponseEntity.ok("Compte cree !");
+    }
+
+
 }
