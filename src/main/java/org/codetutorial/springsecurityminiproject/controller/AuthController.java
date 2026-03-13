960c6345472ae68965b7b0d165615b47f1f9ae2c
Emy-gold <imanelgarate@gmail.com>
2026-03-13 22:05:28 +0000

    finish the api

--- src/main/java/org/codetutorial/springsecurityminiproject/controller/AuthController.java
+++ src/main/java/org/codetutorial/springsecurityminiproject/controller/AuthController.java
@@ -1,6 +1,8 @@
 package org.codetutorial.springsecurityminiproject.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.codetutorial.springsecurityminiproject.dto.AuthRequest;
 import org.codetutorial.springsecurityminiproject.dto.RegisterRequest;
 import org.codetutorial.springsecurityminiproject.model.User;
 import org.codetutorial.springsecurityminiproject.repository.UserRepository;
@@ -8,12 +10,16 @@ import org.codetutorial.springsecurityminiproject.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -39,5 +45,16 @@ public class AuthController {
         return ResponseEntity.ok("Compte cree !");
     }
 
+    //Post /auth/login
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest req){
+    authManager.authenticate(
+            new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword()));
 
+        UserDetails user = userDetailsService.loadUserByUsername(req.getUsername());
+        String token = jwtService.generateToken(user);
+
+        return ResponseEntity.ok(Map.of("token",token));
+
+    }
 }
